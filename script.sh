5353eb83febdd119299e25904d890573ca3467d0
ossama assaghir <59144753+ossamasgr@users.noreply.github.com>
2023-03-06 21:31:02 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Execute Redshift SQL statement and get statement ID
-statementId=$(aws redshift-data execute-statement --region us-east-1 --secret arn:aws:secretsmanager:us-east-1:273940060047:secret:testcluster-9PrWHu --cluster-identi>
+statementId=$(aws redshift-data execute-statement --region us-east-1 --secret arn:aws:secretsmanager:us-east-1:273940060047:secret:testcluster-9PrWHu --cluster-identifier redshift-cluster-1 --database dev --sql "select catid,catname from category"  --query 'Id' --output text)
+
 # Wait for statement execution to complete
 while true; do
     status=$(aws redshift-data describe-statement --region us-east-1 --id $statementId --output json | jq -r '.Status')
@@ -15,22 +16,26 @@ while true; do
     sleep 1
 done
 
-echo "getting results"
+
 
 #statement result
+
 RESULT=$(aws redshift-data get-statement-result --id $statementId --query 'Records[*]' --output json)
-echo "Result: ${RESULT}"
 
-RESULT_ARRAY=$(echo "${RESULT}" | jq -r '.[] | "\(.[0].longValue),\(.[1].stringValue)"' --raw-output)
-echo "Result Array: ${RESULT_ARRAY}"
 
-IFS=$'\n' read -ra RESULT_PAIRS <<< "${RESULT_ARRAY}"
-echo "Start looping"
+RESULT_ARRAY=$(echo "${RESULT}" | jq -r '.[] | "\(.[0].longValue),\(.[1].stringValue)"' --raw-output |  tr '\n' ' ')
+
+IFS=$' ' read -ra RESULT_PAIRS <<< "${RESULT_ARRAY}"
 
 for pair in "${RESULT_PAIRS[@]}"
 do
   IFS=',' read -ra VALUES <<< "${pair}"
-  longValue=${VALUES[0]}
-  stringValue=${VALUES[1]}
-  echo "LongValue: ${longValue}, StringValue: ${stringValue}"
+  trip_id=${VALUES[0]}
+  driver_id=${VALUES[1]}
+  telematics_url="http://telematics-alb.mentor.internal/api/v4/drivers/${trip_id}/trips/${driver_id}?facet=all"
+  echo "downloading file for user trip : ${trip_id} and driver : ${driver_id}"
+  touch  $trip_id.$driver_id
+  echo "ziping file ..."
+  zip -u $trip_id-$driver_id.zip $trip_id.$driver_id
+  echo "zip done" 
 done
