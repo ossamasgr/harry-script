52760948ee135d45bebf8f0aa209c1b2e5d75b89
ossama assaghir <59144753+ossamasgr@users.noreply.github.com>
2023-03-07 10:08:25 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+# Setting Variables
+Database='dev'
+secret='arn:aws:secretsmanager:us-east-1:273940060047:secret:testcluster-9PrWHu'
+region='us-east-1'
+query='select catid,catname from category'
+cluster_identifier='redshift-cluster-1'
 
 # Execute Redshift SQL statement and get statement ID
-statementId=$(aws redshift-data execute-statement --region us-east-1 --secret arn:aws:secretsmanager:us-east-1:273940060047:secret:testcluster-9PrWHu --cluster-identifier redshift-cluster-1 --database dev --sql "select catid,catname from category"  --query 'Id' --output text)
+statementId=$(aws redshift-data execute-statement --region $region --secret $secret --cluster-identifier $cluster_identifier --database $Database --sql "$query"  --query 'Id' --output text)
 
 # Wait for statement execution to complete
 while true; do
-    status=$(aws redshift-data describe-statement --region us-east-1 --id $statementId --output json | jq -r '.Status')
+    status=$(aws redshift-data describe-statement --region $region --id $statementId --output json | jq -r '.Status')
     if [ "$status" == "FINISHED" ]; then
         echo "Statement is finished"
         break
@@ -27,15 +33,33 @@ RESULT_ARRAY=$(echo "${RESULT}" | jq -r '.[] | "\(.[0].longValue),\(.[1].stringV
 
 IFS=$' ' read -ra RESULT_PAIRS <<< "${RESULT_ARRAY}"
 
+# Define function to print progress bar
+function progress_bar() {
+  local progress=$(( $1 * 100 / $2 ))
+  local bar_size=$(( $progress / 2 ))
+  printf "\r[%-${bar_size}s] %d%%" "${bar// /#}" $progress
+}
+# Set up variables for progress bar
+bar="                                                  "
+total=${#RESULT_PAIRS[@]}
+count=0
+
 for pair in "${RESULT_PAIRS[@]}"
 do
   IFS=',' read -ra VALUES <<< "${pair}"
   trip_id=${VALUES[0]}
   driver_id=${VALUES[1]}
   telematics_url="http://telematics-alb.mentor.internal/api/v4/drivers/${trip_id}/trips/${driver_id}?facet=all"
-  echo "downloading file for user trip : ${trip_id} and driver : ${driver_id}"
-  touch  $trip_id.$driver_id
-  echo "ziping file ..."
-  zip -u $trip_id-$driver_id.zip $trip_id.$driver_id
-  echo "zip done" 
+  #curl $telematics_url
+  touch $trip_id.$driver_id
+  zip -u $trip_id-$driver_id.zip $trip_id.$driver_id > /dev/null 2>&1
+  rm $trip_id.$driver_id
+  rm $trip_id-$driver_id.zip
+  count=$(( count + 1 ))
+  progress_bar $count $total
+
 done
+echo " "
+echo -e "Download  \e[0m[\e[32mDone\e[0m]"
+echo -e "Zipping   \e[0m[\e[32mDone\e[0m]"
+echo -e "Uploading \e[0m[\e[32mDone\e[0m]"
